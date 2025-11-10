144e95d336c6d6dce0d750f9cfb9d520bac04c57
AbodiDawood <mo7.daawoud@icloud.com>
2025-11-10 18:28:44 +0100

    Initial Commit

---
+++ CacheSweep/CacheSweepApp.swift
@@ -0,0 +1,15 @@
+//
+//  CacheSweepApp.swift
+//  CacheSweep
+    
+
+import SwiftUI
+
+@main
+struct CacheSweepApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
